f3866049e7bbf924617bcc36dc23d3958239fef3
roman <roman.whale12@gmail.com>
2022-06-23 08:50:31 +0300

    fix: fixed error

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import { Header } from './components/Header';
-=======
 import { LeftMenu } from './components/LeftMenu';
->>>>>>> leftMenu
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       HEllo
-=======
-      <LeftMenu />
->>>>>>> leftMenu
     </div>
   );
 }
